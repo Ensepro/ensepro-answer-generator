1c65da872644f50c755fd78b38e654681d2d13e0
Alencar Rodrigo Hentges <alencarhentges@gmail.com>
2021-11-24 23:04:39 -0300

    saving stats on any error and removing helper from java return

--- src/main/java/com/ensepro/answer/generator/Main.java
+++ src/main/java/com/ensepro/answer/generator/Main.java
@@ -13,7 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
@@ -21,72 +23,105 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        if(args.length > 0 && args[0].equals("version")){
-          System.out.println("base");
-          System.exit(0);
+        if (args.length > 0 && args[0].equals("version")) {
+            System.out.println("base");
+            System.exit(0);
         }
         Configuration config = Configuration.fromArgs(args);
         log.info("####################################################################################");
         log.info("#### STARTING PROCESS: {}", config);
-        JavaResult.JavaResultBuilder resultBuilder = JavaResult.builder();
-        final PythonResult pythonResult = JsonUtil.read2(config.getLoadFile(), PythonResult.class);
+        try {
+            JavaResult.JavaResultBuilder resultBuilder = JavaResult.builder();
+            final PythonResult pythonResult = JsonUtil.read2(config.getLoadFile(), PythonResult.class);
 
-        final List<Triple> triples = new TripleMapper().map(pythonResult.getTriples());
-        final Helper helper = pythonResult.getHelper();
+            final List<Triple> triples = new TripleMapper().map(pythonResult.getTriples());
+            final Helper helper = pythonResult.getHelper();
 
-        log.info("##### phrase: {} ", pythonResult.getPhrase());
-        log.info("####################################################################################");
+            log.info("##### phrase: {} ", pythonResult.getPhrase());
+            log.info("####################################################################################");
 
-        final long startTime = System.nanoTime();
+            final long startTime = System.nanoTime();
 
-        final Score score = Score.builder()
-            .triples(triples)
-            .helper(helper)
-            .config(config)
-            .build();
+            final Score score = Score.builder()
+                    .triples(triples)
+                    .helper(helper)
+                    .config(config)
+                    .build();
 
-        log.info("#### CALCULATING SCORE FOR L1:  size={}", triples.size());
-        score.calculate();
-        log.info("#### CALCULATING SCORE FOR L1 - DONE", triples.size());
+            log.info("#### CALCULATING SCORE FOR L1:  size={}", triples.size());
+            score.calculate();
+            log.info("#### CALCULATING SCORE FOR L1 - DONE", triples.size());
 
-        final AnswerGenerator answerGenerator = AnswerGenerator.builder()
-            .triples(triples)
-            .helper(helper)
-            .config(config)
-            .build();
+            final AnswerGenerator answerGenerator = AnswerGenerator.builder()
+                    .triples(triples)
+                    .helper(helper)
+                    .config(config)
+                    .build();
 
-        log.info("### Generating answer for L1");
-        final List<Answer> answersL1 = answerGenerator.generateL1();
-        log.info("### Generating answer for L1 - DONE - size={}", answersL1.size());
-        resultBuilder.l_size(triples.size());
-        resultBuilder.answer_size(answersL1.size());
-
-        List<Answer> answers = answersL1;
-        if (config.getLevel() > 1) {
-            log.info("### Generating answer for L2");
-            final List<Answer> answersL2 = answerGenerator.generateL2();
-            log.info("### Generating answer for L2 - DONE - size={}", answersL2.size());
-            answers.addAll(answersL2);
+            log.info("### Generating answer for L1");
+            final List<Answer> answersL1 = answerGenerator.generateL1();
+            log.info("### Generating answer for L1 - DONE - size={}", answersL1.size());
             resultBuilder.l_size(triples.size());
-            resultBuilder.answer_size(answersL2.size());
+            resultBuilder.answer_size(answersL1.size());
+
+            List<Answer> answers = answersL1;
+            if (config.getLevel() > 1) {
+                log.info("### Generating answer for L2");
+                final List<Answer> answersL2 = answerGenerator.generateL2();
+                log.info("### Generating answer for L2 - DONE - size={}", answersL2.size());
+                answers.addAll(answersL2);
+                resultBuilder.l_size(triples.size());
+                resultBuilder.answer_size(answersL2.size());
+            }
+
+            log.info("### Sorting answers");
+            Collections.sort(answers);
+            log.info("### Sorting answers");
+
+            answers = answers.stream()
+                    .limit(config.getResultSize())
+                    .collect(Collectors.toList());
+
+            final long endTime = System.nanoTime();
+
+            JsonUtil.save(config.getSaveFile(),
+                    resultBuilder
+                            .answers(answers)
+//                            .helper(helper)
+                            .stats(Collections.emptyMap())
+                            .nanoSeconds(endTime - startTime)
+                            .build()
+            );
+
+
+        } catch (OutOfMemoryError ex) {
+            log.info("ERROR OUT OF MEMORY");
+            final Map<String, Object> stats = new HashMap<>();
+            stats.put("outOutMemory", true);
+            stats.put("exception", ex.getCause().getClass());
+            stats.put("detail", ex.getMessage());
+            JavaResult.JavaResultBuilder resultBuilder = JavaResult.builder();
+            JsonUtil.save(
+                    config.getSaveFile(),
+                    resultBuilder
+                            .answers(Collections.emptyList())
+                            .stats(stats)
+                            .build()
+            );
+        } catch (Exception ex) {
+            log.info("GENERIC ERROR");
+            final Map<String, Object> stats = new HashMap<>();
+            stats.put("outOutMemory", false);
+            stats.put("exception", ex.getCause().getClass());
+            stats.put("detail", ex.getMessage());
+            JavaResult.JavaResultBuilder resultBuilder = JavaResult.builder();
+            JsonUtil.save(
+                    config.getSaveFile(),
+                    resultBuilder
+                            .answers(Collections.emptyList())
+                            .stats(stats)
+                            .build()
+            );
         }
-
-        log.info("### Sorting answers");
-        Collections.sort(answers);
-        log.info("### Sorting answers");
-
-        answers = answers.stream()
-            .limit(config.getResultSize())
-            .collect(Collectors.toList());
-
-        final long endTime = System.nanoTime();
-
-        JsonUtil.save(config.getSaveFile(),
-                resultBuilder
-                .answers(answers)
-                .helper(helper)
-                .nanoSeconds(endTime-startTime)
-                .build()
-        );
     }
 }
